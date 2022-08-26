5dfd440040fb3f5fcc422d1ef7ff7f7bdb370131
asiri-clevertech <asiri.indatissa@clevertech.biz>
2022-08-27 01:56:18 +0530

    Add FuelType

--- FuelApp_SwiftUI/ViewModels/FuelType/AddFuelTypeViewModel.swift
+++ FuelApp_SwiftUI/ViewModels/FuelType/AddFuelTypeViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import CoreData
+
+class AddFuelTypeViewModel:  ObservableObject{
+    @Published var name: String = "diesel"
+    
+    var context: NSManagedObjectContext
+ 
+    
+    init(context: NSManagedObjectContext){
+        self.context = context
+    }
+    
+    func save(){
+        do{
+            let fuelType = FuelType(context: context)
+            fuelType.name = name.lowercased()
+            try fuelType.save()
+        } catch{
+            print(error)
+        }
+    }
+}
