367b8d7ba77da0263e560577f09fc956f093d850
Malith Kuruppu <info.mkappworks@gmail.com>
2022-08-27 14:11:49 +0200

    Changed method to get vehicleType and fuelType in VehicleViewModel

--- FuelApp_SwiftUI/ViewModels/Vehicle/VehicleListViewModel.swift
+++ FuelApp_SwiftUI/ViewModels/Vehicle/VehicleListViewModel.swift
@@ -67,7 +67,7 @@ extension VehicleListViewModel: NSFetchedResultsControllerDelegate{
     }
 }
 
-struct VehicleViewModel: Identifiable{
+struct VehicleViewModel: Identifiable, Hashable{
     private var vehicle: Vehicle
     
     init(vehicle: Vehicle){
@@ -82,13 +82,13 @@ struct VehicleViewModel: Identifiable{
         vehicle.vehicleId ?? ""
     }
     
-//    var vehicleType: String{
-//        vehicle.vehicleType ?? ""
-//    }
-//    
-//    var fuelType: String{
-//        vehicle.fuelType ?? ""
-//    }
+    var vehicleType: String{
+        vehicle.quotas?.vehicleType ?? ""
+    }
+
+    var fuelType: String{
+        vehicle.fuelTypes?.name ?? ""
+    }
     
     
 }
