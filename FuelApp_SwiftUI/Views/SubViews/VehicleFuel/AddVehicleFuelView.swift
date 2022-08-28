b61a68203d785d123b45bef97d6adc9b52c64e77
Malith Kuruppu <info.mkappworks@gmail.com>
2022-08-28 03:19:12 +0200

    Added logic to VehicleFuel views

--- FuelApp_SwiftUI/Views/SubViews/VehicleFuel/AddVehicleFuelView.swift
+++ FuelApp_SwiftUI/Views/SubViews/VehicleFuel/AddVehicleFuelView.swift
@@ -6,15 +6,40 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AddVehicleFuelView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject private var addVehicleFuelVM: AddVehicleFuelViewModel
+    
+    init(vm: AddVehicleFuelViewModel){
+        self.addVehicleFuelVM = vm
+    }
+    
     var body: some View {
-        Text("Add Vehicle Fuel View")
+        VStack{
+            Form{
+                TextField("Vehicle Number", text: $addVehicleFuelVM.vehicleId)
+                    .disabled(true)
+                
+                Section("Enter Pumped Amount"){
+                    TextField("Enter Pumped Amount", value: $addVehicleFuelVM.pumpedAmount, formatter: NumberFormatter())
+                        .keyboardType(.decimalPad)
+                }
+                
+                Button("Save"){
+                    addVehicleFuelVM.save()
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .centerHorizontally()
+                .navigationTitle("Add New Vehicle")
+            }
+        }
+        .alert(addVehicleFuelVM.errorMessage, isPresented: $addVehicleFuelVM.isError) {
+            Button("OK", role: .cancel) {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
     }
-}
-
-struct AddVehicleFuelView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddVehicleFuelView()
-    }
+    
 }
--- FuelApp_SwiftUI/Views/SubViews/VehicleFuel/VehicleFuelListView.swift
+++ FuelApp_SwiftUI/Views/SubViews/VehicleFuel/VehicleFuelListView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct VehicleFuelListView: View {
+    @State private var isPresented: Bool = false
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject private var vehicleFuelListVM: VehicleFuelListViewModel
+    
+    init(vm: VehicleFuelListViewModel){
+        self.vehicleFuelListVM = vm
+    }
+    
+    private func deleteVehicleFuel(at offsets: IndexSet){
+        offsets.forEach { index in
+            let vehicleFuel = vehicleFuelListVM.vehicleFuels[index]
+            vehicleFuelListVM.deleteVehicleFuel(vehicleFuelId: vehicleFuel.id)
+        }
+    }
+    
     var body: some View {
-        Text("Vehicle Fuel List View")
+        NavigationView{
+            VStack{
+                List{
+                    ForEach(vehicleFuelListVM.vehicleFuels){vehicleFuel in
+                        VStack(alignment: .leading){
+                            Text("Date : \(vehicleFuel.date, style: .date)")
+                            Text("Pumped Amount : \(vehicleFuel.pumpedAmount, specifier: "%.2f")")
+                        }
+                    }
+                    .onDelete(perform: deleteVehicleFuel)
+                }
+            }
+            .sheet(isPresented: $isPresented, onDismiss: {
+                //dismiss
+            }, content: {
+                AddVehicleFuelView(vm: AddVehicleFuelViewModel(context: viewContext, vehicle: vehicleFuelListVM.vehicle!))
+            })
+            .navigationTitle("Vehicle Fuel Transaction")
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    ScanButton(scannedText: $vehicleFuelListVM.vehicleId, buttonImageName: "camera.badge.ellipsis", buttonTitle: "Scan Vehicle Number")
+                        .onChange(of: vehicleFuelListVM.vehicleId) {newvalue in
+                            print("check registered")
+                            vehicleFuelListVM.checkVehicleRegistered()
+                        }
+                    
+                    if(vehicleFuelListVM.canPumpFuel){
+                        Button("Pump Fuel to Vehicle"){
+                            isPresented = true
+                        }
+                    }
+                }
+                
+            }
+            .alert(vehicleFuelListVM.errorMessage, isPresented: $vehicleFuelListVM.isError) {
+                Button("OK", role: .cancel) {
+                    //dismiss alert
+                }
+            }
+            
+            
+            
+        }
     }
 }
 
+
+
 struct VehicleFuelListView_Previews: PreviewProvider {
     static var previews: some View {
-        VehicleFuelListView()
+        let viewContext = CoreDataManager.shared.persistenceStoreController.viewContext
+        
+        VehicleFuelListView(vm: VehicleFuelListViewModel(context: viewContext))
     }
 }
